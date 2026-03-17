58f78d8ddea2e4ee291e1422669f43d5b9dc2af4
volvimen <251765543+volvimem@users.noreply.github.com>
2026-03-17 09:48:51 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+// sw.js
+const CACHE_NAME = "boutique-kids-v1";
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './style.css',
+                './app.js',
+                './produtos.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
