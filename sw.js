abab6d38fe1e01e339670a95e817a142e7c34781
volvimen <251765543+volvimem@users.noreply.github.com>
2026-03-23 13:54:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "maribella-kids-v7";
+const CACHE_NAME = "maribella-kids-v8";
 const ASSETS_TO_CACHE = [
     "./",
     "./index.html",
@@ -7,32 +7,25 @@ const ASSETS_TO_CACHE = [
     "./manifest.json"
 ];
 
-// Ouve o evento de instalação e faz o cache dos arquivos vitais
 self.addEventListener('install', (e) => {
     self.skipWaiting();
     e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS_TO_CACHE);
-        }).catch(err => console.log('Erro ao fazer cache', err))
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
     );
 });
 
-// A Faxineira: Apaga TODAS as versões antigas
 self.addEventListener('activate', (e) => {
     e.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
+                    if (cache !== CACHE_NAME) return caches.delete(cache);
                 })
             );
         }).then(() => self.clients.claim())
     );
 });
 
-// Responde às requisições do sistema (necessário pro Chrome liberar a instalação)
 self.addEventListener('fetch', (e) => {
     e.respondWith(
         fetch(e.request).catch(() => caches.match(e.request))
